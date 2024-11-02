aef332320606b379bd008c56b50ddc58ee1bea29
BraydenCh <chawla.brayden@gmail.com>
2024-11-01 21:02:28 -0400

    Firebase accessing changes in messageboard.js
    Implemented basic pulling from firestore

--- my-app/src/messageboard.js
+++ my-app/src/messageboard.js
@@ -1,53 +1,34 @@
 import { useCollection } from 'react-firebase-hooks/firestore';
 import logo from './logo.svg';
 import './App.css';
+import { dB } from './firebase.js';
+import { collection } from 'firebase/firestore';
+
+
+const FirestoreCollection = () => {
+    const usersRef = collection(dB, 'users');
+    const [value, loading, error] = useCollection(usersRef, {
+        snapshotListenOptions: { includeMetadataChanges: true },
+    });
+
+    return (
+        
+            <ul>
+              {value?.docs.map((doc) => (
+                <li key={doc.id}>{doc.data().name} {doc.data().password}</li>
+              ))}
+            </ul>
+        
+    );
+  };
 
 function MessageBoard() {
 
 
-    const FirestoreCollection = () => {
-        const [value, loading, error] = useCollection(
-          firebase.firestore().collection('users'),
-          {
-            snapshotListenOptions: { includeMetadataChanges: true },
-          }
-        );
-        return (
-          <div>
-            <p>
-              {error && <strong>Error: {JSON.stringify(error)}</strong>}
-              {loading && <span>Collection: Loading...</span>}
-              {value && (
-                <span>
-                  Collection:{' '}
-                  {value.docs.map((doc) => (
-                    <React.Fragment key={doc.id}>
-                      {JSON.stringify(doc.data())},{' '}
-                    </React.Fragment>
-                  ))}
-                </span>
-              )}
-            </p>
-          </div>
-        );
-      };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FirestoreCollection/>
     </div>
   );
 }
