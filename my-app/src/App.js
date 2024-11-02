62224515bafae0c9dc29a58e051b00b049ad3d29
Michelle Li <michelleli1510@gmail.com>
2024-11-01 21:05:35 -0400

    added user input for lesson plans

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import LessonInput from './LessonInput.js';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        <LessonInput/>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
