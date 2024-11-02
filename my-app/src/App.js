e80675588e4dab2df7e9951126181742ec1c06e5
druvitharl <druvitharlokasani@gmail.com>
2024-11-01 22:32:02 -0400

    added openAi

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LLMPage from './LLMPage'; // Ensure this path matches where your file is located
+import LLMPage from './LLMPage'; 
+import LessonInput from './LessonInput'; 
 
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/llm" element={<LLMPage />} />
-    //   </Routes>
-    // </Router> 
+
   <div>
     <LLMPage />
+    {/* <LessonInput /> */}
   </div>
   );
 }
