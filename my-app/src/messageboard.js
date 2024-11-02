e3921aa73d82a97cee3062aa3f17ca7843ea961f
BraydenCh <chawla.brayden@gmail.com>
2024-11-01 21:27:30 -0400

    Changed messageboard.js to get data from posts
    Printed at bottom of screen

--- my-app/src/messageboard.js
+++ my-app/src/messageboard.js
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import { dB } from './firebase.js';
 import { collection } from 'firebase/firestore';
+import { format } from 'date-fns';
 
 
 const FirestoreCollection = () => {
-    const usersRef = collection(dB, 'users');
+    const usersRef = collection(dB, 'posts');
     const [value, loading, error] = useCollection(usersRef, {
         snapshotListenOptions: { includeMetadataChanges: true },
     });
@@ -15,7 +16,13 @@ const FirestoreCollection = () => {
         
             <ul>
               {value?.docs.map((doc) => (
-                <li key={doc.id}>{doc.data().name} {doc.data().password}</li>
+                <div><li key={doc.id}>{doc.data().username} {doc.data().title} {doc.data().message} {format(doc.data().time.toDate(), 'MMMM dd, yyyy HH:mm:ss')}</li>
+                    <ul>
+                        {doc.data().replies.map((reply) => (
+                            <li key={reply.id}>{reply.username} {reply.message} {format(reply.time.toDate(), 'MMMM dd, yyyy HH:mm:ss')}</li>
+                        ))}
+                    </ul>
+                </div>
               ))}
             </ul>
         
