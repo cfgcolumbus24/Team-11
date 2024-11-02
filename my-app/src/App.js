365fe473f618b09706ede459fc7a8d11a1548c7f
Has25000 <137649116+Has25000@users.noreply.github.com>
2024-11-01 21:22:57 -0400

    Video Cards Hakeem

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
+import VideoCard from './components/VideoCard';
 
 function App() {
+
+  const videos = [
+    { id: "y_v_G7ub-n0", title: "Research-Backed Strategies for Better Classroom Management" },
+    { id: "EH_C5GyF1sM", title: "Effective Teaching Strategies for the Classroom" },
+    { id: "_jdTtnWMLVM", title: "The 5 Principles of Highly Effective Teachers" },
+    { id: "RlEwNUxLoD4", title: "Effective Lesson Planning: Procedures and Tips" },
+    { id: "lpZ4x93h7Hw", title: "Vocabulary Teaching Strategies" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h2 style={{ marginBottom: '5px' }}>Teaching Strategies Videos</h2>
+    <div style={{ overflowX: "auto", whiteSpace: "nowrap", padding: "10px" }}>
+      {videos.map((video) => (
+        <VideoCard title={video.title} videoId={video.id}></VideoCard>
+      ))}
     </div>
+    </>
   );
 }
 
