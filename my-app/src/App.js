6280dc59e2a749f1d8b2c6b19ca79add12d74e54
Has25000 <137649116+Has25000@users.noreply.github.com>
2024-11-01 21:41:39 -0400

    Merge branch 'feature/homepage' of https://github.com/cfgcolumbus24/Team-11 into feature/homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { Link } from 'react-router-dom';
 import VideoCard from './components/VideoCard';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 
 function App() {
 
@@ -11,15 +14,32 @@ function App() {
     { id: "RlEwNUxLoD4", title: "Effective Lesson Planning: Procedures and Tips" },
     { id: "lpZ4x93h7Hw", title: "Vocabulary Teaching Strategies" },
   ];
-  return (
-    <>
-    <h2 style={{ marginBottom: '5px' }}>Teaching Strategies Videos</h2>
-    <div style={{ overflowX: "auto", whiteSpace: "nowrap", padding: "10px" }}>
-      {videos.map((video) => (
-        <VideoCard title={video.title} videoId={video.id}></VideoCard>
-      ))}
-    </div>
-    </>
+    return (
+        <BrowserRouter>
+        <div className="App">
+           <Header />
+           <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                 Edit <code>src/App.js</code> and save to reload.
+            </p>
+              <h2 style={{ marginBottom: '5px' }}>Teaching Strategies Videos</h2>
+                <div style={{ overflowX: "auto", whiteSpace: "nowrap", padding: "10px" }}>
+                {videos.map((video) => (
+                <VideoCard title={video.title} videoId={video.id}></VideoCard>
+                ))}
+                </div>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+            Learn React
+            </a>
+            </header>
+        </div>
+    </BrowserRouter>
   );
 }
 
