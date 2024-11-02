1f94142f72e025c60b65259e864918b9cd9fa35f
TeddyLupus <l.valeria.lupa@gmail.com>
2024-11-02 06:05:37 -0400

    actually modifies home page

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -16,7 +16,24 @@ function Home() {
                     <p>
                         Welcome to LessonBot! Press the button to start teaching!
                     </p>
-                    
+                <div style={{marginTop: '60px',padding: '20px',backgroundColor: 'rgba(255, 255, 255, 0.1)',borderRadius: '12px',
+                    width: '100%', maxWidth: '1300px', margin: '0 auto', textAlign: 'left', marginBottom: '80px'}}>
+                    <h2 style={{ marginLeft: '30px' }}>
+                        <span style={{ textDecoration: 'underline' }}>How navigate the site</span>
+                    </h2>
+                    <p style={{ fontSize: '4vmin', marginLeft:'50px' }}>
+                        1. Click on the "LessonBot" image to access our lesson plan generation and topic summarization tool.
+                    </p>
+                    <p style={{ fontSize: '4vmin', marginLeft: '50px' }}>
+                       2. Click on 'Resources' in the header to access teaching improvement and lesson planning resources and videos.
+                    </p>
+                    <p style={{ fontSize: '4vmin', marginLeft: '50px' }}>
+                       3. If you're logged in, click on 'Message Board' in the header to access your school's online forum.
+                    </p>
+                    <p style={{ fontSize: '4vmin', marginLeft: '50px' }}>
+                       4. For any further questions click on 'FAQ' in the header to view some common problems.
+                    </p>
+                </div>
            
                 </header>
             </div>
